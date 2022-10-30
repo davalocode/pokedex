6c4b827433ae2d9690803d3d76723cba56146f34
Dani <davalo.code@gmail.com>
2022-10-30 10:18:31 +0100

    .

--- src/generate_pokemon.js
+++ src/generate_pokemon.js
@@ -40,14 +40,45 @@ async function generate() {
 
         //Creacion del tipo
         let tipo=document.createElement("p");
-        tipo.innerHTML=(pokemon.types[0].type.name);
+        tipo.innerHTML=("Type: "+pokemon.types[0].type.name);
         datos_pokemon.append(tipo);
 
         //Creacion de la experiencia
         let experiencia=document.createElement("p");
         experiencia.innerHTML=("Experience: "+pokemon.base_experience);
         datos_pokemon.append(experiencia);
+
+        //Creacion de la vida
+        let hp=document.createElement("p");
+        hp.innerHTML=("HP: "+pokemon.stats[0].base_stat);
+        datos_pokemon.append(hp);
         
+        //Creamos el ataque
+        let attack=document.createElement("p");
+        attack.innerHTML=("attack: "+pokemon.stats[1].base_stat);
+        datos_pokemon.append(attack);
+
+        //Creamos la defensa
+        let defense=document.createElement("p");
+        defense.innerHTML=("Defense: "+pokemon.stats[2].base_stat);
+        datos_pokemon.append(defense);
+
+        //Creamos el ataque especial
+        let special_attack=document.createElement("p");
+        special_attack.innerHTML=("Special attack: "+pokemon.stats[3].base_stat);
+        datos_pokemon.append(special_attack);
+
+        //Creamos la defensa
+        let special_defense=document.createElement("p");
+        special_defense.innerHTML=("Special Defense: "+pokemon.stats[4].base_stat);
+        datos_pokemon.append(special_defense);
+
+        //Creamos la defensa
+        let speed=document.createElement("p");
+        speed.innerHTML=("Speed: "+pokemon.stats[5].base_stat);
+        datos_pokemon.append(speed);
+
+
         //Meter en el html
         contenedor_principal.appendChild(datos_pokemon);
 
